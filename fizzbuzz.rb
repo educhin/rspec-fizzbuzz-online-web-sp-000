5831dd2b53c46401f4cbd35141ae3ebbb2bbd42a
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-13 18:47:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,16 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  end
-  elsif num % 3 == 0
-    "Fizz"
-  end
-  elsif num % 5 == 0
-    "Buzz"
-  end
-  else
-    nil
-  end
+  # if num % 3 == 0 && num % 5 == 0
+  #   "FizzBuzz"
+  # end
+  # elsif num % 3 == 0
+  #   "Fizz"
+  # end
+  # elsif num % 5 == 0
+  #   "Buzz"
+  # end
+  # else
+  #   nil
+  # end
 end
