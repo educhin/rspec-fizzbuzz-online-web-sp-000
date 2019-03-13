9aa0adb35dfe467878f74c79d7fb842ecb863af8
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-13 18:55:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,13 +5,10 @@ def fizzbuzz (num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
   end
-  # elsif num % 3 == 0
-  #   "Fizz"
-  # end
-  # elsif num % 5 == 0
-  #   "Buzz"
-  # end
-  else
-    nil
+  elsif num % 3 == 0
+    "Fizz"
+  end
+  else num % 5 == 0
+    "Buzz"
   end
 end
